29b2532a527418ddf3546ac628d276409fe99ded
AustinKorpi <austingkorpi@gmail.com>
2025-03-06 10:33:00 -0600

    bugz

--- MojaveSphinx.ino
+++ MojaveSphinx.ino
@@ -8,8 +8,8 @@
 const int LOADCELL_DOUT_PIN = 35;
 const int LOADCELL_SCK_PIN = 34;
 
-const int SCALE_LC = -22.19238;
-const int OFFSET_LC = 21503;
+const double SCALE_LC = -22.19238;
+const double OFFSET_LC = 21503;
 
 HX711 scale;
 
@@ -17,8 +17,8 @@ HX711 scale;
 const int PT_TC_PIN = A11;
 const int PT_OX_PIN = A10;
 
-const int SCALE_TC = 1.2195;
-const int OFFSET_TC = - 121.95;
+const double SCALE_TC = 1.2195;
+const double OFFSET_TC = - 121.95;
 
 const int SCALE_OX = 1;
 const int OFFSET_OX = 0;
@@ -59,18 +59,20 @@ void setup() {
   }
   // Write header to file
   myFile.println("Time(ms), LoadCell(g), TCPressure(psi), OXPressure(psi)");
+  myFile.close();
 }
 
 void loop() {
   // Make sensor readings and write to file
+  myFile = SD.open("data.txt", FILE_WRITE);
   myFile.print(millis());
   myFile.print(",");
   myFile.print(scale.get_units(1), 3);
   myFile.print(",");
-  myFile.println(getPressureTC(analogRead(PT_TC_PIN)));
+  myFile.print(getPressureTC(analogRead(PT_TC_PIN)));
   myFile.print(",");
   myFile.println(getPressureOX(analogRead(PT_OX_PIN)));
-
+  myFile.close();
   // Loop as fast as we can
   delay(0);
 }
