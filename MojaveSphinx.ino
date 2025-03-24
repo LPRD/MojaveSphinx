a6fac48af660641de1285fbb681fe0effba42989
AustinKorpi <austingkorpi@gmail.com>
2025-03-24 14:33:32 -0500

    Add beeper

--- MojaveSphinx.ino
+++ MojaveSphinx.ino
@@ -4,9 +4,17 @@
 #include <SPI.h>
 #include <SD.h>
 
+
+// Beeper
+const int BEEPER_PIN = 8;
+const unsigned long beepPeriod = 3000;
+const unsigned long beepLength = 200;
+unsigned long prevTime = 0;
+int currBeepState = 0;
+
 // Load Cell with HX711 breakout board
-const int LOADCELL_DOUT_PIN = 35;
-const int LOADCELL_SCK_PIN = 34;
+const int LOADCELL_DOUT_PIN = 2;
+const int LOADCELL_SCK_PIN = 3;
 
 const double SCALE_LC = -22.19238;
 const double OFFSET_LC = 21503;
@@ -14,8 +22,8 @@ const double OFFSET_LC = 21503;
 HX711 scale;
 
 // Pressure Tranducers
-const int PT_TC_PIN = A11;
-const int PT_OX_PIN = A10;
+const int PT_TC_PIN = A1;
+const int PT_OX_PIN = A0;
 
 const double SCALE_TC = 1.2195;
 const double OFFSET_TC = - 121.95;
@@ -38,6 +46,8 @@ void setup() {
   // Pressure Transducers
   pinMode(PT_TC_PIN, INPUT);
   pinMode(PT_OX_PIN, INPUT);
+  pinMode(BEEPER_PIN, OUTPUT);
+  digitalWrite(BEEPER_PIN, LOW);
 
   // Load Cell
   scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
@@ -63,9 +73,12 @@ void setup() {
 }
 
 void loop() {
+  // Sets up millis
+  unsigned long currTime = millis();
+
   // Make sensor readings and write to file
   myFile = SD.open("data.txt", FILE_WRITE);
-  myFile.print(millis());
+  myFile.print(currTime);
   myFile.print(",");
   myFile.print(scale.get_units(1), 3);
   myFile.print(",");
@@ -74,5 +87,16 @@ void loop() {
   myFile.println(getPressureOX(analogRead(PT_OX_PIN)));
   myFile.close();
   // Loop as fast as we can
-  delay(0);
+  //delay(0);
+  
+  // Beeper Intervals
+  if (currTime - prevTime >= beepPeriod) {
+    digitalWrite(BEEPER_PIN, HIGH);
+    prevTime = currTime;
+    currBeepState = 1;
+  }
+  if (currTime - prevTime >= beepLength && currBeepState == 1) {
+    digitalWrite(BEEPER_PIN, LOW);
+    currBeepState = 0;
+  }
 }
